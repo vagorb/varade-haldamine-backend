6f36634f45ac74d15e7e666f7641870ce0761af1
Janar Keit Jaakson <jjaaks@ttu.ee>
2021-03-01 15:04:43 +0200

    Initial

---
+++ src/main/java/ee/taltech/varadehaldamine/Varadehaldamine/VaradeHaldamineApplication.java
@@ -0,0 +1,13 @@
+package ee.taltech.varadehaldamine.Varadehaldamine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaradeHaldamineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaradeHaldamineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/taltech/varadehaldamine/Varadehaldamine/VaradeHaldamineApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.taltech.varadehaldamine.Varadehaldamine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaradeHaldamineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
