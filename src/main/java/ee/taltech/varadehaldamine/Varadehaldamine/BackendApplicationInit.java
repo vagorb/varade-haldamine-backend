1cc16008d8cf77ce79fde57c276ea320df35ced6
vagorb <vagorb@ttu.ee>
2021-03-03 20:08:33 +0200

    added postgresql

---
+++ src/main/java/ee/taltech/varadehaldamine/Varadehaldamine/BackendApplicationInit.java
@@ -0,0 +1,18 @@
+package ee.taltech.varadehaldamine.Varadehaldamine;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+public class BackendApplicationInit implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) {
+
+    }
+}
+
---
+++ src/main/java/ee/taltech/varadehaldamine/Varadehaldamine/repository/VaradeRepository.java
@@ -0,0 +1,4 @@
+package ee.taltech.varadehaldamine.Varadehaldamine.repository;
+
+public interface VaradeRepository {
+}
