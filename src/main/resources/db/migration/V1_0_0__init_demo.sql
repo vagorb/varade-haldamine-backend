51119da8fc6120681a1a894aebef1de31cbc1e79
alrudo <alrudo@ttu.ee>
2021-03-17 21:09:41 +0200

    Cleanup

--- src/main/resources/db/migration/V1_0_0__init_demo.sql
+++ src/main/resources/db/migration/V1_0_0__init_demo.sql
@@ -1,20 +1,3 @@
--- CREATE TABLE IF NOT EXISTS `varadehaldamine`.`asset` (
--- `id` INT NOT NULL AUTO_INCREMENT ,
--- `name` VARCHAR(255) NOT NULL ,
--- `value_id` INT NOT NULL ,
--- `description` VARCHAR(255) NULL ,
--- `subclass` INT NOT NULL ,
--- `active` TINYINT NOT NULL ,
--- `address_id` INT NULL DEFAULT NULL ,
--- `user_id` INT NULL DEFAULT NULL ,
--- `owner_id` INT NOT NULL ,
--- `accured` INT NOT NULL ,
--- `expiration_date` DATE NULL ,
--- `delicate_condition` TINYINT NOT NULL ,
--- `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
--- `modified_at` DATETIME on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
---  PRIMARY KEY (`id`)) ENGINE = InnoDB;
-
 CREATE TABLE IF NOT EXISTS demo_table (
     id SERIAL PRIMARY KEY,
     alt_id VARCHAR(52),
