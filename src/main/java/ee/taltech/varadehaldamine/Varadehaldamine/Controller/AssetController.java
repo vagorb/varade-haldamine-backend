43cb6a619dda03efe20d65054baa63454b5d2c8a
Ilja Boitsuk <ilboit@ttu.ee>
2021-03-30 13:44:48 +0300

    change requestparam to pathvariable

--- src/main/java/ee/taltech/varadehaldamine/Varadehaldamine/Controller/AssetController.java
+++ src/main/java/ee/taltech/varadehaldamine/Varadehaldamine/Controller/AssetController.java
@@ -25,21 +25,56 @@ public class AssetController {
         return assetService.findAll();
     }
 
-    @GetMapping("search")
-    public List<AssetInfo> getAllSearch(@RequestParam(value = "name", required = false) String name,
-                                        @RequestParam(value = "active", required = false) Boolean active,
-                                        @RequestParam(value = "user", required = false) Long userId,
-                                        @RequestParam(value = "direction", defaultValue = "MAX") String direction){
-        return null;
-    }
+//    @GetMapping
+//    public List<AssetInfoShort> getAll(@RequestParam(value = "name", required = false) String name,
+//                                       @RequestParam(value = "active", required = false) Boolean active,
+//                                       @RequestParam(value = "user", required = false) Long userId,
+//                                       @RequestParam(value = "possessor", required = false) Long possessorId,
+//                                       @RequestParam(value = "delicate", required = false) Boolean delicateCondition,
+//                                       @RequestParam(value = "subclass", required = false) String subclass,
+//                                       @RequestParam(value = "class", required = false) String mainClass,
+//                                       @RequestParam(value = "building", required = false) String building,
+//                                       @RequestParam(value = "institute", required = false) Integer institute,
+//                                       @RequestParam(value = "division", required = false) Integer division,
+//                                       @RequestParam(value = "subdivision", required = false) Integer subdivision) {
+//        if (name != null) {
+//            //some
+//        }
+//        if (active != null) {
+//            //some
+//        }
+//        if (userId != null) {
+//            //some
+//        }
+//        if (possessorId != null) {
+//            //some
+//        }
+//        if (delicateCondition != null) {
+//            //some
+//        }
+//        if (subclass != null) {
+//            //some
+//        }
+//        if (mainClass != null) {
+//            //some
+//        }
+//        if (building != null) {
+//            //some
+//        }
+//        if (institute != null) {
+//            //some
+//        }
+//        return assetService.findAll();
+//    }
 
-    @GetMapping("id")
-    public AssetInfo getAssetById(@RequestParam String assetId) {
-        return assetService.getAssetById(assetId);
+
+    @GetMapping("{id}")
+    public AssetInfo getAssetById(@PathVariable String id) {
+        return assetService.getAssetById(id);
     }
 
     @PostMapping
-    public ResponseEntity<Object> addAsset(@RequestBody AssetInfo asset){
+    public ResponseEntity<Object> addAsset(@RequestBody AssetInfo asset) {
         if (assetService.addAsset(asset) != null) {
             return ResponseEntity.status(HttpStatus.CREATED).build();
         }
