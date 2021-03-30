c8a6e2ffacbe9d9899aa0b459e10611af0a62150
Janar Keit Jaakson <jjaaks@ttu.ee>
2021-03-30 20:30:57 +0300

    Initial 3 "objects" for 3 different users

---
+++ src/main/resources/db/migration/V1_0_1__init_insert.sql
@@ -0,0 +1,21 @@
+INSERT INTO Person(azure_id, firstname, lastname) VALUES ('WW1234561', 'VASJA', 'GORB');
+INSERT INTO Possessor(institute, division, subdivision) VALUES (112, 111, 110);
+INSERT INTO Classification (sub_class, main_class) VALUES ('hiir', 'arvutikomponent');
+INSERT INTO Asset (id, name, sub_class, user_id, possessor_id) VALUES ('AK1120', 'Arvutihiir HP 2210', 'hiir', 1, 1);
+INSERT INTO Worth (asset_id, price, residual_price) VALUES ('AK1120', 15.00, 15.00);
+INSERT INTO Address (asset_id, building_abbreviature, room) VALUES ('AK1120', 'SOC', '203');
+INSERT INTO Kit_relation (component_asset_id, major_asset_id) VALUES ('AK1120', 'AK1120');
+
+INSERT INTO Person(azure_id, firstname, lastname) VALUES ('WW192883', 'Niki', 'Birj');
+INSERT INTO Classification (sub_class, main_class) VALUES ('klaviatuur', 'arvutikomponent');
+INSERT INTO Asset (id, name, sub_class, user_id, possessor_id) VALUES ('AK1121', 'Arvutiklaviatuur HP 2210', 'klaviatuur', 2, 1);
+INSERT INTO Worth (asset_id, price, residual_price) VALUES ('AK1121', 30.00, 30.00);
+INSERT INTO Address (asset_id, building_abbreviature, room) VALUES ('AK1121', 'SOC', '203');
+INSERT INTO Kit_relation (component_asset_id, major_asset_id) VALUES ('AK1121', 'AK1121');
+
+INSERT INTO Person(azure_id, firstname, lastname) VALUES ('WW111111', 'Art', 'Hur');
+INSERT INTO Classification (sub_class, main_class) VALUES ('monitor', 'arvutikomponent');
+INSERT INTO Asset (id, name, sub_class, user_id, possessor_id) VALUES ('AK1122', 'Arvutimonitor HP 2210', 'monitor', 3, 1);
+INSERT INTO Worth (asset_id, price, residual_price) VALUES ('AK1122', 55.00, 55.00);
+INSERT INTO Address (asset_id, building_abbreviature, room) VALUES ('AK1122', 'SOC', '203');
+INSERT INTO Kit_relation (component_asset_id, major_asset_id) VALUES ('AK1122', 'AK1122');
