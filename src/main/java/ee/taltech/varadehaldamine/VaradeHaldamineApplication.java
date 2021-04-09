e22d0be892f504132fa719d149a01570c05b9b42
vagorb <vagorb@ttu.ee>
2021-04-09 20:22:31 +0300

    azure

--- src/main/java/ee/taltech/varadehaldamine/VaradeHaldamineApplication.java
+++ src/main/java/ee/taltech/varadehaldamine/VaradeHaldamineApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 @SpringBootApplication
 public class VaradeHaldamineApplication {
 
--- src/main/java/ee/taltech/varadehaldamine/controller/AssetController.java
+++ src/main/java/ee/taltech/varadehaldamine/controller/AssetController.java
@@ -9,26 +9,35 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientId;
 import org.springframework.web.bind.annotation.*;
 
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.security.Principal;
 import java.util.List;
 
 @RequestMapping("asset")
 @RestController
 public class AssetController {
 
+
+    @PreAuthorize("hasRole('ROLE_admin')")
+    @GetMapping("/account")
+    public Object getAccount() {
+        return SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+
     @Autowired
     AssetService assetService;
 
-    @PreAuthorize("hasRole('ROLE_admin')")
-    @RequestMapping("/admin")
-    public String groupOne() {
-        return "Hello Group 1 Users!";
-    }
-
     @GetMapping("/filtered")
     @ResponseBody
-    @PreAuthorize("hasRole('ROLE_admin')")
     public ResponseEntity<Page<AssetInfoShort>> getAssets(
             AssetInfoShort assetSearchCriteria,
             @RequestParam(required = false, value = "page", defaultValue = "0") int page,
@@ -41,19 +50,16 @@ public class AssetController {
     }
 
     @GetMapping
-    @PreAuthorize("hasRole('ROLE_admin')")
     public List<AssetInfoShort> getAll() {
         return assetService.findAll();
     }
 
     @GetMapping("/{id}")
-    @PreAuthorize("hasRole('ROLE_admin')")
     public AssetInfo getAssetById(@PathVariable String id) {
         return assetService.getAssetById(id);
     }
 
     @PostMapping
-    @PreAuthorize("hasRole('ROLE_admin')")
     public ResponseEntity<Object> addAsset(@RequestBody AssetInfo asset) {
         if (assetService.addAsset(asset) != null) {
             return ResponseEntity.status(HttpStatus.CREATED).build();
