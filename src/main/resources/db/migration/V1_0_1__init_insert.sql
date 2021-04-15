2aa4648c4df796d8d77394c590d11c1be80a98f7
Janar Keit Jaakson <jjaaks@ttu.ee>
2021-04-15 22:27:13 +0300

    Asset Redo, Fixed sql tables and inserts

--- src/main/resources/db/migration/V1_0_1__init_insert.sql
+++ src/main/resources/db/migration/V1_0_1__init_insert.sql
@@ -11,4 +11,23 @@ INSERT INTO Classification (sub_class, main_class) VALUES ('VV_ARVUTIKOMPLEKT', 
 INSERT INTO Classification (sub_class, main_class) VALUES ('PV_LAUD', 'PV');
 INSERT INTO Classification (sub_class, main_class) VALUES ('PV_KAPP', 'PV');
 
+INSERT INTO Asset (id, name, sub_class,user_id, possessor_id, expiration_date, building_abbreviature, room, price, residual_price, description)
+VALUES ('AAKP00001', 'Klassikapp 1', 'PV_KAPP', 3, 4, '2021-05-31', 'U02', '103', 150.00, 150.00, 'Kapp on, rohkemat ei vaja');
+INSERT INTO Comment (asset_id, text) VALUES ('AAKP00001', 'Hea kapp');
+INSERT INTO Comment (asset_id, text) VALUES ('AAKP00001', 'Mulle ei meeldi');
+INSERT INTO Comment (asset_id, text) VALUES ('AAKP00001', 'Internetti pole');
+
+INSERT INTO Asset (id, name, sub_class, possessor_id, expiration_date, building_abbreviature, room, price, residual_price)
+VALUES ('AAKP00002', 'Klassikapp 2', 'PV_KAPP', 4, '2021-05-31', 'U02', '103', 130.00, 130.00);
+
+INSERT INTO Asset (id, name, sub_class, possessor_id, expiration_date, building_abbreviature, room, price, residual_price)
+VALUES ('AAKP00003', 'Klassikapp 3', 'PV_KAPP', 4, '2021-05-31', 'U02', '103', 120.00, 120.00);
+
+INSERT INTO Asset (id, name, sub_class, possessor_id, expiration_date, building_abbreviature, room, price, residual_price)
+VALUES ('AAKP00004', 'Klassikapp 4', 'PV_KAPP', 4, '2021-05-31', 'U02', '103', 100.00, 100.00);
+
+INSERT INTO Asset (id, name, sub_class, possessor_id, expiration_date, building_abbreviature, room, description)
+VALUES ('LD01', 'Laud ilus', 'PV_LAUD', 3, '2024-09-01', 'U01', '228', 'Väga hea laud, meeldib kuidas see särab päikese käes...');
+
+
 
